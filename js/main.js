a1f87187449945463eb4ba84cc829d38e3976515
1y38a6 <40470289+1y38a6@users.noreply.github.com>
2019-04-10 14:53:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 $(".menu-toggle").click(menuToggle);
 $(function(){
     $(".mb-menu").slideToggle();
-    var reviews = $(".review-image");
+
+    // Струткура: кликабельный блок>эллементы
+    var reviews = $(".review-listener");
 
     for(var a = 0; a<reviews.length; a++){
-        $(reviews[a]).addClass("r-img-" + a);
-        $(reviews[a]).nextAll(".review-text").addClass("r-txt-" + a);
-        $(reviews[a]).nextAll(".review-age").addClass("r-age-" + a);
+        $(reviews[a]).addClass("r-" + a);
+        $(reviews[a]).find(".review-image").addClass("r-img-" + a);
+        $(reviews[a]).find(".review-text").addClass("r-txt-" + a);
+        $(reviews[a]).find(".review-age").addClass("r-age-" + a);
         
         reviews[a].addEventListener("click", OpenReview);
 
         CloseReview(reviews[a]);
+    }
+
+
+    // Блог
+
+    var articles = $(".blog-listener");
+
+    for(var a = 0; a<articles.length; a++){
+        
+        articles[a].addEventListener("click", OpenArticle);
+
+        CloseArticle(articles[a]);
+    }
+
+    var sliders = $(".slider");
+    for(var a = 0; a<sliders.length; a++){
+        sliders[a].addEventListener("click", SlideToNext);
     }
 });
 
@@ -28,22 +48,23 @@ window.onscroll = function() {
 }
 
 function OpenReview(){
+    
     if(screen.width > 768){
         return
     }
     var str = "";
     if(this.className != undefined){
         str = this.className;
-        if($(this).hasClass("review-image-left")){
-            $(this).removeClass("review-image-left");
+        
+        if($(this).find(".review-image").hasClass("review-image-left")){
+            $(this).find(".review-image").removeClass("review-image-left");
             str = this.className;
             str = str.substring(str.length-1);
             $( ".r-age-" +str).addClass("invisible");
-            //str = str.substring(str.length-18, str.length-17);
         }else{
             str = str.substring(str.length-1);
             $( ".r-age-" +str).removeClass("invisible");
-            $(this).addClass("review-image-left");
+            $(this).find(".review-image").addClass("review-image-left");
         }
     }
     
@@ -57,16 +78,15 @@ function CloseReview(elem){
     var str = "";
     if(elem.className != undefined){
         str = elem.className;
-        if($(elem).hasClass("review-image-left")){
-            $(elem).removeClass("review-image-left");
+        if($(elem).find(".review-image").hasClass("review-image-left")){
+            $(elem).find(".review-image").removeClass("review-image-left");
             str = elem.className;
             str = str.substring(str.length-1);
             $( ".r-age-" +str).addClass("invisible");
-            //str = str.substring(str.length-18, str.length-17);
         }else{
             str = str.substring(str.length-1);
             $( ".r-age-" +str).removeClass("invisible");
-            $(elem).addClass("review-image-left");
+            $(elem).find(".review-image").addClass("review-image-left");
         }
     }
     
@@ -85,4 +105,42 @@ function menuToggle(){
     }
     
     $(".mb-menu").slideToggle( "slow" );
+}
+
+function OpenArticle(){
+    if(this.className != undefined){
+        
+        if($(this).find(".blog-image").hasClass("blog-image-deactive")){
+            $(this).find(".blog-image").removeClass("blog-image-deactive");
+            $(this).find(".blog-title").removeClass("blog-title-deactive");
+        }else{
+            $(this).find(".blog-title").addClass("blog-title-deactive");
+            $(this).find(".blog-image").addClass("blog-image-deactive");
+        }
+
+        $(this).find(".blog-text").slideToggle( "slow" );
+    }
+}
+
+function CloseArticle(elem){
+    if(elem.className != undefined){
+        
+        if($(elem).find(".blog-image").hasClass("blog-image-deactive")){
+            $(elem).find(".blog-image").removeClass("blog-image-deactive");
+            $(elem).find(".blog-title").removeClass("blog-title-deactive");
+        }else{
+            $(elem).find(".blog-title").addClass("blog-title-deactive");
+            $(elem).find(".blog-image").addClass("blog-image-deactive");
+        }
+
+        $(elem).find(".blog-text").slideToggle( "slow" );
+    }
+}
+
+function SlideToNext(){
+    var sections = $(this).nextAll("section");
+
+    $('html, body').animate({
+        scrollTop: $(sections[1]).offset().top
+    }, 1000);
 }
